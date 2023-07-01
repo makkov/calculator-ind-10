59d1e077a9c98a89b75dd7bba4a0423c82b1e6b7
maks <maxim_86nva@mail.ru>
2023-07-01 09:17:54 +0300

    init project

---
+++ src/main/java/com/example/calculatorind10/CalculatorInd10Application.java
@@ -0,0 +1,13 @@
+package com.example.calculatorind10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorInd10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorInd10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorind10/CalculatorInd10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorind10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorInd10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
